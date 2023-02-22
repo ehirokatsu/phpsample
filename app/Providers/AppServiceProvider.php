6ec7c11ad35abe598422cf54c0ad7abd92c33df9
hiro <48009332+ehirokatsu@users.noreply.github.com>
2023-02-22 20:20:05 +0900

    サービスコンテナ

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Http\Services\HelloService;
+use App\Http\Services\HelloService2;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+
+
+
+
+        //$this->app->bind(Hello::class,'App\Http\Services\HelloService');
+
+        //use無しでおk。makeで呼び出すならこれで。
+        //$this->app->bind('HelloService', function(){
+    
+        //use必須。コンストラクタインジェクションではこっち
+        $this->app->bind(HelloService::class, function(){
+    
+            $class = new \App\Http\Services\HelloService();
+            $class->foo = 'bar';
+            return $class;
+        });
+
+        
     }
 
     /**
