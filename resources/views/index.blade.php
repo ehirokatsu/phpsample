7aa813e178f533cafdf2fe0f9a1370735990546f
hiro <48009332+ehirokatsu@users.noreply.github.com>
2023-04-05 10:41:55 +0900

    CBC BASIC1 骨組み

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -26,19 +26,94 @@
 
 <main class="main">
   main
+  <section class="main__kv">
+    main__kv
+  </section>
+  <section class="main__about">
+    main__about
+    <h1 class="main__about--ttl">title</h1>
+    <div class="main__about--logo"><img src="" alt=""></div>
+    <p class="main__about--txt">text</p>
+  </section>
+  <section class="main__service">
+    main__service
+    <div class="main__service--message">
+      <h2>h2 title</h2>
+      <p>text</p>
+    </div>
+    <ul class="main__service--list">
+      <li>
+        <a href="">
+          <img src="" alt="">
+          <h3>title</h3>
+          <p>text</p>
+        </a>
+      </li>
+      <li>
+        <a href="">
+          <img src="" alt="">
+          <h3>title</h3>
+          <p>text</p>
+        </a>
+      </li>
+      <li>
+        <a href="">
+          <img src="" alt="">
+          <h3>title</h3>
+          <p>text</p>
+        </a>
+      </li>
+    </ul>
+  </section>
+  <section class="main__news">
+    main__news
+    <h2 class="main__news--ttl">h2 title</h2>
+    <ul class="main__news--list">
+      main__news--list
+      <li>
+        <a href="">
+          <dl class="main__news--item">
+            <dt class="main__news--day">2017</dt>
+            <dd class="main__news--data">text</dd>
+          </dl>
+        </a>
+      </li>
+      <li>
+        <a href="">
+          <dl class="main__news--item">
+            <dt class="main__news--day">2018</dt>
+            <dd class="main__news--data">text</dd>
+          </dl>
+        </a>
+      </li>
+      <li>
+        <a href="">
+          <dl class="main__news--item">
+            <dt class="main__news--day">2019</dt>
+            <dd class="main__news--data">text</dd>
+          </dl>
+        </a>
+      </li>
+    </ul>
+  </section>
 </main>
 <footer class="footer">
   footer
   <div class="footer__logo"><img src="" alt=""></div>
   <div class="footer__copyright">copy</div>
 </footer>
+
+
 <section>
   section
   <span>
     aiueo
   </span>
   <div class="test">
-    abcde
+    test
+    <div class="test2">test2</div>
+    <div class="test3">test3</div>
+
   </div>
   <span>
     aiueo
