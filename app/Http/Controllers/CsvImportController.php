bc317d41f6b8088e4c9c67e6ccd8cd113d31506f
hiro <hiro>
2023-05-10 15:30:46 +0900

    微修正

--- app/Http/Controllers/CsvImportController.php
+++ app/Http/Controllers/CsvImportController.php
@@ -3,10 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-use Illuminate\Support\Facades\Storage;
 use App\Models\Board;
-//use Illuminate\Support\Facades\DB;
 
 class CsvImportController extends Controller
 {
@@ -42,21 +39,21 @@ class CsvImportController extends Controller
         }
 
         //保存したファイルを取得する
-        $csv = Storage::disk('local')->get("public/csv/{$newCsvFileName}");
+        $csv = \Storage::disk('local')->get("public/csv/{$newCsvFileName}");
 
         //異なる改行コードを統一する
         $csv = str_replace(array("\r\n","\r"), "\n", $csv);
-        //dump($csv);
+
         $uploadedData = collect(explode("\n", $csv));
-        //dd($uploadedData);
+
         $board = new Board();
 
         //boardテーブルのカラム
         $header = collect($board->csvHeader());
-        //dump($header);
+
         //アップロードしたCSVの1行目を取得しカンマで区切ってコレクション化する
         $uploadedHeader = collect(explode(",", $uploadedData->shift()));
-        //dd($uploadedHeader);
+
         if ($header->count() !== $uploadedHeader->count()) {
             
             throw new Exception('Error: ヘッダーが一致しません');
